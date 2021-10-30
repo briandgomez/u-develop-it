ff47f07fc250b7e2bed30b264d982e07d7590661
briandgomez <briandgomez98@gmail.com>
2021-10-30 17:30:03 -0400

    test connection by querying database

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-//Default response for any other request (Not Found) LAST ROUTE
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
+
+//Default response for any other request (Not Found) ***LAST ROUTE***
 app.use((req, res) => {
     res.status(404).end();
 });
