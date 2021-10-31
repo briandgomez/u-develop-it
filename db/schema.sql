59b4a097b3efe17c2d4e303615772ac8160401a7
briandgomez <briandgomez98@gmail.com>
2021-10-31 15:43:07 -0400

    add candidate route to change their party

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE parties (
   description TEXT
 );
 
-CREATE TABLE candidates(
+CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
